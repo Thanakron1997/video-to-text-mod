65d7db5e759697b3abe9d95431ddf8bc361ca01e
THN1997 <thanakron.nop@gmail.com>
2024-03-31 17:10:50 +0700

    upload

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd video2text_mod
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs
 git clone https://huggingface.co/mmaaz60/LLaVA-7B-Lightening-v1-1
